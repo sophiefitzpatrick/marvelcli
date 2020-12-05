22332fa5dd640f77fa19bc015d5e01e3b2ceebe6
Sophie Fitzpatrick <sophierfitzpatrick@gmail.com>
2020-12-05 14:36:09 +0000

    tidy up

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
-# from setuptools import setup
+from setuptools import setup
 
 setup(
-    name="marvel-cli",
+    name="marvelcli",
     version="0.1",
     author='Sophie Fitzpatrick',
    	author_email='sophierfitzpatrick@gmail.com',
@@ -9,7 +9,7 @@ setup(
     install_requires=['click', 'requests'],
     entry_points={
         "console_scripts": [
-            "marvel-cli = cliproject.__main__:main"
+            "marvelcli = cliproject.__main__:main"
         ]
     },
 )
